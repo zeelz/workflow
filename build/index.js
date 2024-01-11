55ad2d0f86e2dfd0b240215646d5625f7d0cb29f
jprodyx <jprodyx@gmail.com>
2024-01-11 08:12:35 +0100

    chore:app-setup remove static, return json

--- build/index.js
+++ build/index.js
@@ -5,6 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.use(express_1.default.static('static'));
-const PORT = 3344;
-app.listen(PORT, () => console.log(`Running at localhost:${PORT}`));
+// app.use(express.static('frontend'))
+app.use(express_1.default.json());
+const PORT = 3366;
+app.get("/", (req, res) => {
+    return res.status(200).send({ "msg": "canada 2024" });
+});
+app.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`));
