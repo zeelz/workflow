1f73eee8eb65141001b84a013a1f6228375800b8
jprodyx <jprodyx@gmail.com>
2024-01-11 09:06:14 +0100

    chore:app-setup add github workflows

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ app.use(express.json())
 const PORT = 3366
 
 app.get("/", (req: Request, res: Response) => {
-    return res.status(200).send({"msg": "canada 2024"})
+    return res.status(200).send({"msg": "Canada 2024"})
 })
 
 app.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`))
