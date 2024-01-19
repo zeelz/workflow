f94f79ec0890cc1798dd745f3c6dec16b9ca3d9f
jprodyx <jprodyx@gmail.com>
2024-01-19 13:12:55 +0100

    ci: update test workflow
    Forcefully exit test runner after test

--- test.ts
+++ test.ts
@@ -3,8 +3,10 @@ import request from "supertest"
 
 describe("Test suite", () => {
     it("/test to return 200", async () => {
-        const res = await request(app).get("/test")
-        console.log("Response object:", res.body);
+        const res = await request(app)
+            .get("/test")
+            
+        // console.log("Response object:", res.body);
         expect(res.statusCode).toEqual(200)
     })
 })
