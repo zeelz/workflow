3833ecb4220babc2d673b52021bd514ff31bbd64
jprodyx <jprodyx@gmail.com>
2024-02-08 14:29:42 +0100

    ci:test-action run test on pr

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app: Express = express()
 // app.use(express.static('frontend'))
 app.use(express.json())
 
-const PORT = 3366
+const PORT = process.env.PORT || 3366
 
 app.get("/test", (req: Request, res: Response) => {
     return res.status(200).send({"goal": "Canada 2024"})
